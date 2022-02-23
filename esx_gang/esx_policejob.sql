9fbd9f06619f33626ffa9bc37ed7186f423e0df0
jmilan24 <98666492+jmilan24@users.noreply.github.com>
2022-02-23 22:24:39 +0100

    Add files via upload

---
+++ esx_gang/esx_policejob.sql
@@ -0,0 +1,23 @@
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_changeme', 'changeme', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('society_changeme', 'changeme', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_changeme', 'changeme', 1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('changeme', 'change me')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('changeme',0,'recruit','cahngeme',20,'{}','{}'),
+	('changeme',1,'officer','changeme',40,'{}','{}'),
+	('changeme',2,'sergeant','changeme',60,'{}','{}'),
+	('changeme',3,'lieutenant','changeme',85,'{}','{}'),
+	('changeme',4,'boss','changeme',100,'{}','{}')
+;
